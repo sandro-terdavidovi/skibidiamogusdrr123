646798ccabb427bf169a48bde61c86c9ec253a7f
sandro-terdavidovi <terdavidovisandro5@gmail.com>
2025-06-07 19:50:35 +0300

    commit

---
+++ day 07/cw/cw.py
@@ -0,0 +1,25 @@
+num1 = 5 
+num2 = 20 
+sum = 20 + 5 
+print(sum) 
+
+
+price = 50 
+price = 80 
+print(price) 
+
+
+
+name = input("enter your name")  
+age = input("input your age") 
+
+
+
+
+
+#debbuging-არის შეცდომის გასწორება 
+
+
+
+
+#კომენტარები იყენება როცა შენ პროეკტს ბევრი დრო აკეთებ და გავიწყდება კოდი ას აკეთბს ან ჯგუფური პროეკტებში
