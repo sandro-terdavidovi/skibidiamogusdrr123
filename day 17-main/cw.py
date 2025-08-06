0dd6522ee7bf4fbdd80f81fe582a3c916b96dca2
sandro-terdavidovi <terdavidovisandro5@gmail.com>
2025-08-06 17:16:14 +0300

    commit

---
+++ day 17-main/cw.py
@@ -0,0 +1,36 @@
+#product = 1
+#if product == 1:
+    #print("regular price")
+#else:
+    #print("discount")
+
+seats = 10
+while seats > 0:
+    print("sandro")
+    seats = seats - 1
+
+
+i = 0
+while i <= 10:
+    print(i)
+    i = i + 1
+
+
+
+number = 0
+while number <= 100: 
+    print("number")
+    number = number + 1
+    
+age = 18 
+if age == 18:
+    print("regular price")
+else: 
+    print("discount")
+
+
+age = input("enter your age")
+if age == 18: 
+    print("good luck using our app!")
+else:
+    print("sorry you cant enter app")
---
+++ day 17-main/minicw.py
@@ -0,0 +1,17 @@
+seats = 10
+while seats > 0:
+    print("sandro")
+    seats = seats - 1
+
+
+i = 0
+while i <= 10:
+    print(i)
+    i = i + 1
+
+
+
+number = 0
+while number <= 100: 
+    print("number")
+    number = number + 1
