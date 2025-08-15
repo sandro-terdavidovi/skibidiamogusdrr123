b1d44f38e4fd053db6c45d4e6584f57e3fa3dfed
sandro-terdavidovi <terdavidovisandro5@gmail.com>
2025-08-15 03:02:57 +0300

    commit

---
+++ day 13/cw.py
@@ -0,0 +1,25 @@
+for skibidi in range(1,10) :  
+            print(skibidi) 
+
+
+for skibidi in range(100) : 
+        print("sandro")  
+
+name = "sandro" 
+for f in name : 
+  print(f) 
+
+for i in range(0, 21, 2):
+    print(i)
+
+
+#ფოორ ლუპი იყენება რომ დაპრინტო რაგაცა ტეკსტი რამდენიმე ჯერ შეიძლება სიტყვები და რიცხვები გაყო რო ამეების ასოები ან ციფრები მიიღო ასევე შეგიძლია რიცხვი გაყო და მერე ციფრები შეადარო სხვა ციფრს 
+
+
+for i in range(0, 21, 2):
+    print(i) 
+
+
+for i in range(75, 150, 1):
+    print(i)  
+
